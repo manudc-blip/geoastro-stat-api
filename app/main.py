913e8ea999a20fc144fa6ad09a61f90949ad8d68
manudc-blip <manu.dc@wanadoo.fr>
2026-05-22 17:05:20 +0200

    Allow Vercel frontend in CORS

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://geoastro-stat-mhwild7l9-manuel-dcs-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
